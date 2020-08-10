136e98ecd0f4472752c630cebd8103fe4434354e
amit <amit.dror@milab.idc.ac.il>
2020-08-10 15:35:10 +0300

    added hands and made asset directory cleaner

---
+++ Assets/Scripts/GimiButtonTimedAnimation.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class GimiButtonTimedAnimation : MonoBehaviour {
+
+    public new Animation animation;
+
+    void Start()
+    {
+        animation = GetComponent<Animation>();
+        StartCoroutine(MyCoroutine());
+    }
+
+    void RepeatMyCoroutine()
+    {
+        StartCoroutine(MyCoroutine());
+    }
+
+    private IEnumerator MyCoroutine()
+    {
+        animation.Play("ClipName");
+        yield return new WaitForSeconds(5f);
+        animation.Stop("ClipName");
+        RepeatMyCoroutine();
+        yield return null;
+    }
+    /*
+        // Use this for initialization
+        public Animation Uni_Animation_GimiB_move;
+        public float f_threshhold; //trigger pf movement
+        public int I_Timer;
+        private bool b_TimePass;
+
+
+        // Use this for initialization
+        void Start()
+        {
+            Uni_Animation_GimiB_move = GetComponent<Animation>();
+            b_TimePass = false;
+            InvokeRepeating("b_TimePassSwitch", 0f, 10f);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Uni_Animation_GimiB_move.isPlaying||b_TimePass)
+            {
+                return;
+            }
+            float distance = GetDistanceFromGimi();
+            if (distance < f_threshhold )
+            {
+                Uni_Animation_GimiB_move.Play();
+                //yield wait(Timer);
+                Uni_Animation_GimiB_move.Rewind(); 
+            }
+        }
+
+        public float GetDistanceFromGimi()
+        {
+            float distance = Vector3.Distance(this.transform.position, Camera.main.transform.position);
+            return distance;
+        }
+
+        public void b_TimePassSwitch()
+        {
+            b_TimePass=!b_TimePass;
+        }*/
+}
---
+++ Assets/Scripts/gimi_jonathan_lookat.cs
@@ -0,0 +1,112 @@
+﻿// C#
+using System;
+using UnityEngine;
+
+
+public class gimi_jonathan_lookat : MonoBehaviour {
+
+    #region possion and time parameters
+    
+    public Transform target;
+    public float timedelay = .5f;
+    Quaternion orginalPosition;
+    [SerializeField]
+    bool B_shouldJiggle = false;
+    private float time;
+    private Vector3 oldPos;
+    private float timmerForJiggle;
+
+    #endregion
+
+
+    #region jitter function parameters 
+
+    [Range(0, 1)]
+    public float power = .1f;
+    [Header("Position Jiggler")]
+    public Vector3 positionJigAmount;
+    [Range(0, 120)]
+    public float positionFrequency = 10;
+    float positionTime;
+    Vector3 basePosition;
+    #endregion
+
+
+    private void Awake() {
+       
+    }
+
+    private void Start() {
+        orginalPosition = transform.rotation;
+        basePosition = this.transform.localPosition;
+        oldPos = transform.position;
+        timmerForJiggle = 1;
+    }
+
+    void Update() {
+        time += Time.deltaTime;
+        timmerForJiggle += Time.deltaTime;
+        positionTime += time * positionFrequency;
+
+        if (Vector3.Distance(this.transform.position,oldPos)>1.0f)//moved
+        {
+            oldPos = transform.position;
+            B_shouldJiggle = true;
+            timmerForJiggle = 0;
+            transform.LookAt(target);
+        }
+        else {
+            if(timmerForJiggle < 0.1f)//finished move jiggle time delay
+            {
+                transform.LookAt(target);
+                this.transform.localPosition = basePosition + positionJigAmount * Mathf.Sin(positionTime) * power;
+            }
+            else
+            {
+                transform.LookAt(target);
+            }
+        }
+        
+
+       
+    }
+
+
+
+    #region not used fundtions
+    void SmoothLookAt(Vector3 newDirection)
+    {
+        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(newDirection - transform.position), Time.deltaTime * 3);
+    }
+
+
+    /// <summary>
+    /// maybe usefull in the future
+    /// </summary>
+    /// <param name="looker"></param>
+    /// <param name="targetPos"></param>
+    /// <param name="FOVAngle"></param>
+    /// <returns></returns>
+    bool IsLookingAtObject(Transform looker, Vector3 targetPos, float FOVAngle)
+    {
+
+        Vector3 direction = targetPos - looker.position;
+        float ang = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        float lookerAngle = looker.eulerAngles.z;
+        float checkAngle = 0f;
+
+        if (ang >= 0f)
+            checkAngle = ang - lookerAngle - 90f;
+        else if (ang < 0f)
+            checkAngle = ang - lookerAngle + 270f;
+
+        if (checkAngle < -180f)
+            checkAngle = checkAngle + 360f;
+
+        if (checkAngle <= FOVAngle * .5f && checkAngle >= -FOVAngle * .5f)
+            return true;
+        else
+            return false;
+    }
+    #endregion
+}
