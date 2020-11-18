ef8e0cd5995d208fc7ce26b232fb70b72e22866e
amitdrormilab <amit.dror@milab.idc.ac.il>
2020-11-18 17:43:45 +0200

    new scaryRoom with sceneChanger

---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using static OVRInput;
+
+public class SceneChanger : MonoBehaviour
+{
+    public Controller controller;
+    public void ChangeScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    // Update is called once per frame
+    public void Exit()
+    {
+        Application.Quit(); 
+    }
+}
