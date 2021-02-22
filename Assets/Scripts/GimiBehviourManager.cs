467aed22136e34a53fea23b907c20250ebc6a917
amitdrormilab <amit.dror@milab.idc.ac.il>
2021-02-22 15:15:31 +0200

    one gimi 2 behaviors for testing

--- Assets/Scripts/GimiBehviourManager.cs
+++ Assets/Scripts/GimiBehviourManager.cs
@@ -7,7 +7,7 @@ public class GimiBehviourManager : MonoBehaviour
     private float distance;
     private float time = 0.0f;
     private float startsleeptime = 0f;
-    public Transform HDM;
+    public Transform HMD;
     public float transitionSpeedToStart = 3.0f;
     public Transform AnimationStart;
     public Transform IntervalStart;
@@ -26,8 +26,7 @@ public class GimiBehviourManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        distance = (float)Vector3.Distance(this.transform.position, HDM.position);
-        //        Debug.Log("distance is : " + distance);
+        distance = (float)Vector3.Distance(this.transform.position, HMD.position);
         if (distance >= SocialDistance + bufferDistance)//animation distance
         {
             Debug.Log("if animation pass");
@@ -35,12 +34,12 @@ public class GimiBehviourManager : MonoBehaviour
             return;
         }
 
-        if (distance > IntimateDistance + bufferDistance && distance < SocialDistance - bufferDistance)//interval distance
+        /*if (distance > IntimateDistance + bufferDistance && distance < SocialDistance - bufferDistance)//interval distance
         {
             Debug.Log("if interval pass");
             enableAndDisableScripts(false);
             return;
-        }
+        }*/
         if (distance <= IntimateDistance - bufferDistance)
         {
             Debug.Log("if stalker pass");
@@ -52,10 +51,10 @@ public class GimiBehviourManager : MonoBehaviour
             Debug.Log("_____In Buffer__________");
             if (currentRunningBehavior == null)
             {
-                SmoothLookAt(AnimationStart.position, transitionSpeedToStart, Time.deltaTime);
+                SmoothLookAt(HMD.position, transitionSpeedToStart);
                 Debug.Log("_______looking to animation___________");
             }
-            if (currentRunningBehavior == true) { SmoothLookAt(HDM.position, transitionSpeedToStart, Time.deltaTime); }
+            if (currentRunningBehavior == true) { SmoothLookAt(AnimationStart.position, transitionSpeedToStart); }
 
             this.GetComponent<GimiInterval>().enabled = false;
             this.GetComponent<Animator>().enabled = false;
@@ -64,65 +63,165 @@ public class GimiBehviourManager : MonoBehaviour
             bufferBool = true;
 
         }
-    }
+        void enableAndDisableScripts(bool? wantedState)
+        {
+            if (currentRunningBehavior == wantedState && !bufferBool)
+            {
+                //Debug.Log("same state");
+                return;
+            }
+            if (wantedState == null)
+            {
+                //Debug.Log("animation activate");
+                SmoothLookAt(AnimationStart.position, transitionSpeedToStart);
+                this.GetComponent<GimiStalker>().enabled = false;
+                this.GetComponent<GimiInterval>().enabled = false;
 
-    void SmoothLookAt(Vector3 newDirection, float speed, float startMovementTime)
-    {
-        time += Time.deltaTime;
-        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(newDirection - transform.position), Time.deltaTime * speed);
-        if (time <= (startMovementTime + 3f))
-        {
-            //transform.LookAt(startPoint);
-        }
-    }
+                //this.GetComponent<GimiLookatToAnimation>().enabled = true;
+                this.GetComponent<Animator>().enabled = true;
+                bufferBool = false;
+                currentRunningBehavior = null;
+                return;
+            }
+            /*if (wantedState == false)
+            {
+                //Debug.Log("interval activate");
+                this.GetComponent<GimiStalker>().enabled = false;
+                this.GetComponent<Animator>().Play("AmitGimi animation", -1, normalizedTime: 0.0f);
+                this.GetComponent<Animator>().enabled = false;
+                //this.GetComponent<GimiLookatToAnimation>().enabled = false;
+                SmoothLookAt(IntervalStart.position, transitionSpeedToStart, Time.deltaTime);
+                this.GetComponent<GimiInterval>().enabled = true;
+                currentRunningBehavior = false;
+                bufferBool = false;
+                return;
+            }*/
+            if (wantedState == true)
+            {
+                //Debug.Log("stalking activate");
+                SmoothLookAt(HMD.transform.position, transitionSpeedToStart);
+                this.GetComponent<GimiInterval>().enabled = false;
+                this.GetComponent<Animator>().enabled = false;
+                this.GetComponent<Animation>().enabled = false;
+                //this.GetComponent<GimiLookatToAnimation>().enabled = false;
+                this.GetComponent<GimiStalker>().enabled = true;
+                currentRunningBehavior = true;
+                bufferBool = false;
+                return;
+            }
 
-    void enableAndDisableScripts(bool? wantedState)
-    {
-        if (currentRunningBehavior == wantedState && !bufferBool)
-        {
-            //Debug.Log("same state");
-            return;
         }
-        if (wantedState == null)
-        {
-            //Debug.Log("animation activate");
-            SmoothLookAt(AnimationStart.position, transitionSpeedToStart, Time.deltaTime);
-            this.GetComponent<GimiStalker>().enabled = false;
-            this.GetComponent<GimiInterval>().enabled = false;
 
-            //this.GetComponent<GimiLookatToAnimation>().enabled = true;
-            this.GetComponent<Animator>().enabled = true;
-            bufferBool = false;
-            currentRunningBehavior = null;
-            return;
-        }
-        if (wantedState == false)
+        void SmoothLookAt(Vector3 newDirection, float speed)
         {
-            //Debug.Log("interval activate");
-            this.GetComponent<GimiStalker>().enabled = false;
-            this.GetComponent<Animator>().Play("AmitGimi animation", -1, normalizedTime: 0.0f);
-            this.GetComponent<Animator>().enabled = false;
-            //this.GetComponent<GimiLookatToAnimation>().enabled = false;
-            SmoothLookAt(IntervalStart.position, transitionSpeedToStart, Time.deltaTime);
-            this.GetComponent<GimiInterval>().enabled = true;
-            currentRunningBehavior = false;
-            bufferBool = false;
-            return;
-        }
-        if (wantedState == true)
-        {
-            //Debug.Log("stalking activate");
-            this.GetComponent<GimiInterval>().enabled = false;
-            this.GetComponent<Animator>().enabled = false;
-            this.GetComponent<Animation>().enabled = false;
-            //this.GetComponent<GimiLookatToAnimation>().enabled = false;
-            SmoothLookAt(HDM.transform.position, transitionSpeedToStart, Time.deltaTime);
-            this.GetComponent<GimiStalker>().enabled = true;
-            currentRunningBehavior = true;
-            bufferBool = false;
-            return;
+            Debug.Log("smooth look at");
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(newDirection - transform.position), Time.deltaTime * speed);
+            
         }
 
 
+
+
+        #region 3 part gimi
+        /* distance = (float)Vector3.Distance(this.transform.position, HMD.position);
+         //        Debug.Log("distance is : " + distance);
+         if (distance >= SocialDistance + bufferDistance)//animation distance
+         {
+             Debug.Log("if animation pass");
+             enableAndDisableScripts(null);
+             return;
+         }
+
+         if (distance > IntimateDistance + bufferDistance && distance < SocialDistance - bufferDistance)//interval distance
+         {
+             Debug.Log("if interval pass");
+             enableAndDisableScripts(false);
+             return;
+         }
+         if (distance <= IntimateDistance - bufferDistance)
+         {
+             Debug.Log("if stalker pass");
+             enableAndDisableScripts(true);//stalking distace
+             return;
+         }
+         else
+         {
+             Debug.Log("_____In Buffer__________");
+             if (currentRunningBehavior == null)
+             {
+                 SmoothLookAt(AnimationStart.position, transitionSpeedToStart, Time.deltaTime);
+                 Debug.Log("_______looking to animation___________");
+             }
+             if (currentRunningBehavior == true) { SmoothLookAt(HMD.position, transitionSpeedToStart, Time.deltaTime); }
+
+             this.GetComponent<GimiInterval>().enabled = false;
+             this.GetComponent<Animator>().enabled = false;
+             this.GetComponent<GimiStalker>().enabled = false;
+             this.GetComponent<Animator>().Play("AmitGimi animation", -1, normalizedTime: 0.0f);
+             bufferBool = true;
+
+         }
+     }
+
+     void SmoothLookAt(Vector3 newDirection, float speed, float startMovementTime)
+     {
+         time += Time.deltaTime;
+         transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(newDirection - transform.position), Time.deltaTime * speed);
+         if (time <= (startMovementTime + 3f))
+         {
+             //transform.LookAt(startPoint);
+         }
+     }
+
+     void enableAndDisableScripts(bool? wantedState)
+     {
+         if (currentRunningBehavior == wantedState && !bufferBool)
+         {
+             //Debug.Log("same state");
+             return;
+         }
+         if (wantedState == null)
+         {
+             //Debug.Log("animation activate");
+             SmoothLookAt(AnimationStart.position, transitionSpeedToStart, Time.deltaTime);
+             this.GetComponent<GimiStalker>().enabled = false;
+             this.GetComponent<GimiInterval>().enabled = false;
+
+             //this.GetComponent<GimiLookatToAnimation>().enabled = true;
+             this.GetComponent<Animator>().enabled = true;
+             bufferBool = false;
+             currentRunningBehavior = null;
+             return;
+         }
+         if (wantedState == false)
+         {
+             //Debug.Log("interval activate");
+             this.GetComponent<GimiStalker>().enabled = false;
+             this.GetComponent<Animator>().Play("AmitGimi animation", -1, normalizedTime: 0.0f);
+             this.GetComponent<Animator>().enabled = false;
+             //this.GetComponent<GimiLookatToAnimation>().enabled = false;
+             SmoothLookAt(IntervalStart.position, transitionSpeedToStart, Time.deltaTime);
+             this.GetComponent<GimiInterval>().enabled = true;
+             currentRunningBehavior = false;
+             bufferBool = false;
+             return;
+         }
+         if (wantedState == true)
+         {
+             //Debug.Log("stalking activate");
+             this.GetComponent<GimiInterval>().enabled = false;
+             this.GetComponent<Animator>().enabled = false;
+             this.GetComponent<Animation>().enabled = false;
+             //this.GetComponent<GimiLookatToAnimation>().enabled = false;
+             SmoothLookAt(HMD.transform.position, transitionSpeedToStart, Time.deltaTime);
+             this.GetComponent<GimiStalker>().enabled = true;
+             currentRunningBehavior = true;
+             bufferBool = false;
+             return;
+         }
+        
+
+    } */
+        #endregion
     }
 }
